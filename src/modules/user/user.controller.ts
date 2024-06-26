312bf42fb4c31a4223ff27efee9b3b649da788a6
Mehedi <mehedi@etlimited.net>
2024-06-26 19:19:34 +0600

    removed unnecessay codes

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,17 +1,12 @@
-/* eslint-disable no-console */
+import httpStatus from 'http-status';
 import { RequestHandler } from '../../utils';
 import { UserServices } from './user.service';
 import sendResponse from '../../utils/sendResponse';
-import httpStatus from 'http-status';
-// import studentValidationSchema from '../student/student.validator';
 import catchAsync from '../../utils/catchAsync';
 
 const createStudent: RequestHandler = catchAsync(async (req, res) => {
   const { password, student: studentData } = req.body;
-  console.table(req.body)
-  // const parsedStudentData = studentValidationSchema.parse(studentData);
   const result = await UserServices.createStudentIntoDB(studentData, password);
-  console.table(result)
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
