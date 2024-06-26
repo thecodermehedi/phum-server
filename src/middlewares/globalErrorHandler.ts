8157466676a78d4584569fd7722f8e419b543ee9
Mehedi <mehedi@etlimited.net>
2024-06-26 19:17:55 +0600

    fix: error message not showing properly

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,16 +1,30 @@
-import { Request, Response } from '../utils';
+import { ZodError } from 'zod';
+import { ErrorRequestHandler } from '../utils';
 
-const globalErrorHandler = (
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  err: any,
-  req: Request,
-  res: Response,
+const globalErrorHandler: ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  next
 ) => {
-  return res.status(500).json({
-    success: false,
-    message: err.message || 'Something went wrong',
-    error: err,
-  });
+  if (err instanceof ZodError) {
+    const errorMessages =
+      err.issues.length === 1
+        ? err.issues[0].message
+        : err.issues.map((issue) => issue.message).join(', ');
+    res.status(400).json({
+      success: false,
+      message: `${err.issues.length > 1 ? `${err.issues.length} validation error(s): ` : ''}${errorMessages}`,
+      errors: err.issues,
+    });
+  } else {
+    return res.status(500).json({
+      success: false,
+      message: 'Something went wrong',
+      error: err,
+    });
+  }
 };
 
 export default globalErrorHandler;
