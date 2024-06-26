f577d60cdccbeb6573c24373c8edeeed7c5b993d
Mehedi <mehedi@etlimited.net>
2024-06-26 19:18:39 +0600

    created a central middleware to validate all request

---
+++ src/middlewares/validateRequest.ts
@@ -0,0 +1,15 @@
+import { AnyZodObject } from "zod"
+import { RequestHandler } from "../utils"
+
+const validateRequest = (schema: AnyZodObject): RequestHandler => {
+  return async (req, res, next) => {
+    try {
+      await schema.parseAsync({ body: req.body })
+      next()
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+export default validateRequest
