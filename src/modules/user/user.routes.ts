3694b916495483f4a7d2704761af4f4f3b4e1026
Mehedi <mehedi@etlimited.net>
2024-06-26 19:19:47 +0600

    added validateRequest middleware

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,8 +1,10 @@
+import validateRequest from '../../middlewares/validateRequest';
 import createRouter from '../../utils/createRouter';
+import { studentValidations } from '../student/student.validator';
 import { UserControllers } from './user.controller';
 
 const router = createRouter();
 
-router.post('/create-student', UserControllers.createStudent);
+router.post('/create-student', validateRequest(studentValidations.studentValidationSchema), UserControllers.createStudent);
 
 export const UserRoutes = router;
