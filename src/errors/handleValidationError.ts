def8d200aad12b3d5de21bc6fea416b83d9b67ae
Mehedi <mehedi@etlimited.net>
2024-07-04 18:59:05 +0600

    updated

---
+++ src/errors/handleValidationError.ts
@@ -0,0 +1,19 @@
+import { TErrorObject, TReturnError } from "../types";
+import { httpStatus, mongoose } from "../utils";
+
+type TMongooseError = mongoose.Error.ValidatorError | mongoose.Error.CastError
+
+const handleValidationError = (err: mongoose.Error.ValidationError): TReturnError => {
+  return {
+    code: httpStatus.BAD_REQUEST,
+    message: 'Mongoose Error',
+    details: Object.values(err.errors).map((issue: TMongooseError): TErrorObject => {
+      return {
+        path: issue?.path,
+        message: issue?.message,
+      };
+    }),
+  }
+}
+
+export default handleValidationError;
