95b5a82ac3350425d13e268dd0714b32581b2d4c
Mehedi <mehedi@etlimited.net>
2024-07-04 17:35:54 +0600

    adding all kinds of error checks and making it the perfect globalErrorHandler

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,30 +1,34 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+// This file sends error responses to client
+
 import { ZodError } from 'zod';
 import { ErrorRequestHandler, httpStatus } from '../utils';
+import handleZodError from '../errors/handleZodError';
+import { TErrorObject, TResponse } from '../types';
+import getCurrentDateTime from '../utils/getCurrentDateTime';
+import config from '../config';
 
-const globalErrorHandler: ErrorRequestHandler = (
-  err,
-  req,
-  res,
-  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-  next,
-) => {
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode: number = httpStatus.INTERNAL_SERVER_ERROR;
+  let statusMessage: string = 'Something went wrong'
+  let statusDetails: Array<TErrorObject> = [{ path: '', message: 'Something went wrong' }]
+
   if (err instanceof ZodError) {
-    const errorMessages =
-      err.issues.length === 1
-        ? err.issues[0].message
-        : err.issues.map((issue) => issue.message).join(', ');
-    res.status(httpStatus.BAD_REQUEST).json({
-      success: false,
-      message: `${err.issues.length > 1 ? `${err.issues.length} validation error(s): ` : ''}${errorMessages}`,
-      errors: err.issues,
-    });
-  } else {
-    return res.status(err.statusCode || httpStatus.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      message: err.message || 'Something went wrong',
-      error: Object.keys(err).length === 0 ? null : err,
-    });
-  }
+    const simplifiedError = handleZodError(err)
+    statusCode = simplifiedError.code;
+    statusMessage = simplifiedError.message;
+    statusDetails = simplifiedError.details
+  } 
+
+  return res.status(statusCode).json({
+    status: 'error',
+    message: statusMessage,
+    details: statusDetails,
+    timestamp: getCurrentDateTime(),
+    ...(config.nodeEnv !== 'production' ? { debugInfo: { method: req.method ?? 'no method provided', url: req.url ?? 'no url provided', stack: err.stack ?? 'no stack provided' } } : {}),
+  })
 };
 
 export default globalErrorHandler;
