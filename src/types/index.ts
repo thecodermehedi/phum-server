94ba0bf841486b8e11547b1f7d385faa83bf67cf
Mehedi <mehedi@etlimited.net>
2024-07-04 17:33:29 +0600

    renamed to index

---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+export type TStatus = 'success' | 'error' | 'created' | 'no_content'
+
+export type TErrorObject = {
+  path: string | number,
+  message: string
+};
+
+type TDebugInfo = {
+  method: string;
+  url: string;
+  stack?: string;
+};
+
+export type TResponse<T> = {
+  status: TStatus;
+  code: number;
+  message: string;
+  data?: T;
+  errorCode?: string;
+  details?: Array<TErrorObject>;
+  timestamp?: string;
+  debugInfo?: TDebugInfo
+};
+
+export type TReturnError = {
+  code: number,
+  message: string,
+  details: Array<TErrorObject>
+}
