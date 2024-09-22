f28ec98b0b70f95ebc191d8e8e710d7583c8343e
Mehedi Hasan <iammehedihasan@protonmail.com>
2024-09-22 08:14:15 +0600

    Add authentication middleware to user routes

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,26 +1,32 @@
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import createRouter from '../../utils/createRouter';
 import { AdminValidations } from '../Admin/admin.validator';
 import { FacultyValidations } from '../Faculty/faculty.validator';
 import { StudentValidations } from '../student/student.validator';
+import { USER_ROLE } from './user.constant';
 import { UserControllers } from './user.controller';
 
 const router = createRouter();
 
 router.post(
   '/create-student',
+  auth(USER_ROLE.admin),
   validateRequest(StudentValidations.createStudentValidationSchema),
   UserControllers.createStudent,
 );
 
 router.post(
   '/create-faculty',
+  auth(USER_ROLE.admin),
   validateRequest(FacultyValidations.createFacultyValidationSchema),
   UserControllers.createFaculty,
 );
 
 router.post(
   '/create-admin',
+  //TODO: auth middleware will be commented out until we have a super admin
+  // auth(USER_ROLE.admin),
   validateRequest(AdminValidations.createAdminValidationSchema),
   UserControllers.createAdmin,
 );
