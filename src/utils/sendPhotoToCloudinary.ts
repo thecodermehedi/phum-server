1d1c7520fec3566251d89c1bd486a704ed5108a1
Mehedi Hasan <iammehedihasan@protonmail.com>
2024-09-27 08:53:11 +0600

    refactor: Rearrange config properties for better organization and add cloudinary configuration

---
+++ src/utils/sendPhotoToCloudinary.ts
@@ -0,0 +1,12 @@
+import { v2 as cloudinary } from 'cloudinary';
+import config from '../config';
+
+const sendPhotoToCloudinary = () => {
+  cloudinary.config({
+    cloud_name: config.cloudinaryCloudName,
+    api_key: config.cloudinaryApiKey,
+    api_secret: config.cloudinaryApiSecret,
+  });
+};
+
+export default sendPhotoToCloudinary;
